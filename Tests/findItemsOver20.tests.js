4fbbe50cb5aafcbb562cca39ad6d420a50fbe701
mbali <nkabindembali3@gmail.com>
2022-04-28 12:22:15 +0200

    Added test functions

--- Tests/findItemsOver20.tests.js
+++ Tests/findItemsOver20.tests.js
@@ -1,12 +1,14 @@
 describe('The findItemsOver20 function', function () {
     
-        it("should return all item over20 qty", function(){
-         assert.equal(findItemsOver20 ['pears ,qty  37' , 'bananas,qty 27'])
-        })
-        it("should return all item over20 qty", function(){
+        it("should return the names of all item over20 in qty when 'findItemsOver20' is passed", function(){
         assert.equal(findItemsOver20  [
             {name : 'apples', qty : 40},{name : 'apples', qty : 37}, 
-            {name : 'pears', qty : 20}, {name : 'bananas', qty : 23}  ]) 
-        
-           })
+            {name : 'pears', qty : 20}, {name : 'bananas', qty : 23} ]) 
+        })
+        it("should return all item with over20 qty from the string when 'findItemsOver20' is passed", function(){
+            assert.equal(findItemsOver20 ['pears ,qty  37' , 'bananas,qty 27'])
+        })
+        it("should return 0 when thereare noitems with over20 qty from the string when 'findItemsOver20' is passed", function(){
+            assert.equal(findItemsOver20 ['apples ,qty  14' , 'bananas,qty 8'])
+        })
     });
--- Tests/mostProfitableDepartment.tests.js
+++ Tests/mostProfitableDepartment.tests.js
@@ -1,28 +1,55 @@
-let dataList = [
-  {department : 'Computers', sales : 4500, day : 'Monday'},
-  {department : 'Gardening', sales : 1500, day : 'Monday'},
-  {department : 'Computers', sales : 5500, day : 'Monday'},
-  {department : 'Gardening', sales : 7500, day : 'Tuesday'},
-  {department : 'Computers', sales : 2505, day : 'Tuesday'},
-  {department : 'Furniture', sales : 1540, day : 'Tuesday'}
-]
+var dataList = [
+  {department : 'hardware', sales : 4500, day : 'Monday'},
+  {department : 'outdoor', sales : 1500, day : 'Monday'},
+  {department : 'carpentry', sales : 5500, day : 'Monday'},
+  {department : 'hardware', sales : 7500, day : 'Tuesday'},
+  {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+  {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+  {department : 'hardware', sales : 1500, day : 'Wednesday'},
+  {department : 'outdoor', sales : 8507, day : 'Wednesday'},
+  {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+  {department : 'hardware', sales : 12000, day : 'Thursday'},
+  {department : 'outdoor', sales : 18007, day : 'Thursday'},
+  {department : 'carpentry', sales : 6109, day : 'Thursday'},
+  {department : 'hardware', sales : 7005, day : 'Friday'},
+  {department : 'outdoor', sales : 12006, day : 'Friday'},
+  {department : 'carpentry', sales : 16109, day : 'Friday'},
+];
 
-let dataList2 = [
-  {department : 'IT', sales : 1000, day : 'Monday'},
-  {department : 'Finance', sales : 5000, day : 'Monday'},
-  {department : 'Finance', sales : 1000, day : 'Monday'},
-  {department : 'Operations', sales : 7500, day : 'Tuesday'},
-  {department : 'Finance', sales : 1500, day : 'Tuesday'},
-  {department : 'Operations', sales : 7000, day : 'Tuesday'}
-]
+var dataList2 = [
+  {department : 'electronics', sales : 4500, day : 'Monday'},
+  {department : 'outdoor', sales : 1500, day : 'Monday'},
+  {department : 'carpentry', sales : 5500, day : 'Monday'},
+  {department : 'steelwork', sales : 7500, day : 'Tuesday'},
+  {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+  {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+  {department : 'hardware', sales : 1500, day : 'Wednesday'},
+  {department : 'outdoor', sales : 8507, day : 'Wednesday'},
+  {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+  {department : 'hardware', sales : 12000, day : 'Thursday'},
+  {department : 'carpentry', sales : 6109, day : 'Thursday'},
+  {department : 'hardware', sales : 7005, day : 'Friday'},
+  {department : 'electronics', sales : 12006, day : 'Friday'},
+  {department : 'electronics', sales : 16109, day : 'Saturday'},
+  {department : 'steelwork', sales : 7500, day : 'Tuesday'},
+  {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+  {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+  {department : 'steelwork', sales : 1500, day : 'Wednesday'},
+  {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+];
+
 
 describe('Test my mostProfitableDepartment',function(){
-  it('It should return "Computers" as the most profitable department',function(){
-      assert.equal('Computers',mostProfitableDepartment(dataList));
+  it('It should return "outdoor" as the most profitable department',function(){
+      assert.equal('outdoor',mostProfitableDepartment(dataList));
   })
-
-  it('It should return "Operations" as the most profitable department',function(){
-      assert.equal('Operations',mostProfitableDepartment(dataList2));
+  it('It should return "electronics" as the most profitable department',function(){
+      assert.equal('electronics',mostProfitableDepartment(dataList2));
   })
-  
+  it('It should return "Thursday" as the most profitable day',function(){
+    assert.equal('Thursday',mostProfitableDay(dataList));
+  })
+  it('It should return "Wednesday" as the most profitable day',function(){
+  assert.equal('Wednesday',mostProfitableDay(dataList2));
+  })
 })
